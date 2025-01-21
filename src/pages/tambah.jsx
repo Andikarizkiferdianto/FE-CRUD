c06a752fbf232536f1c1d137be9257576bec4753
DixaorAngsigMa <AndikaRizkiFerdianto.TKJ1.24@binusasmg.sch.id>
2025-01-21 16:00:48 +0700

    update commit

--- src/pages/tambah.jsx
+++ src/pages/tambah.jsx
@@ -6,42 +6,119 @@ import axios from "axios";
 import { API_DUMMY } from "../utils/base_url";
 import Swal from "sweetalert2";
 import { useNavigate } from "react-router-dom";
+import { uploadImageToS3 } from "../utils/uploadImageToS3";
 
 function Add() {
   const [name, setName] = useState("");
   const [type, setType] = useState("");
   const [price, setPrice] = useState("");
   const [deskripsi, setDeskripsi] = useState("");
-  const [link_gambar, setGambar] = useState("");
+  const [link_gambar, setGambar] = useState(null);
+
   const navigate = useNavigate();
 
   const addData = async (e) => {
     e.preventDefault(); // Menghindari reload halaman
 
-    try {
-      await axios.post(`${API_DUMMY}/api/menus`, {
-        name: name, // Properti sebelah kiri dari API, sebelah kanan dari state
-        price: price,
-        type: type,
-        deskripsi: deskripsi,
-        link_gambar: link_gambar,
-      });
+    // Validasi input
+    if (!name || !type || !price || !deskripsi) {
       Swal.fire({
-        icon: "success",
-        title: "Data berhasil ditambahkan!!",
+        icon: "warning",
+        title: "Harap isi semua field!",
         showConfirmButton: false,
         timer: 1500,
       });
-      setTimeout(() => {
-        navigate("/menu");
-      }, 1500);
+      return;
+    }
+
+    try {
+      let imageUrl = "";
+
+      // Upload gambar jika ada
+      if (link_gambar) {
+        try {
+          imageUrl = await uploadImageToS3(link_gambar);
+        } catch (err) {
+          Swal.fire({
+            icon: "error",
+            title: "Gagal mengunggah gambar!",
+            text: err.message || "Terjadi kesalahan saat upload gambar.",
+            showConfirmButton: false,
+            timer: 1500,
+          });
+          return; // Keluar dari fungsi jika upload gambar gagal
+        }
+      }
+
+      // Kirim data ke API
+      const response = await axios.post(
+        `${API_DUMMY}/api/menus`,
+        {
+          name: name,
+          price: price,
+          type: type,
+          deskripsi: deskripsi,
+          link_gambar: imageUrl,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        }
+      );
+      
+      console.log(response.data);  // Tambahkan ini untuk memeriksa response
+      
+      if (response.data.code === 200 || response.data.code === 201) {
+        Swal.fire({
+          icon: "success",
+          title: "Data berhasil ditambahkan!!",
+          showConfirmButton: false,
+          timer: 1500,
+        });
+        setTimeout(() => {
+          navigate("/menu");
+        }, 1500);
+      } else {
+        Swal.fire({
+          icon: "success",
+          title: "Data berhasil ditambahkan!!",
+          showConfirmButton: false,
+          timer: 1500,
+        });
+        setTimeout(() => {
+          navigate("/menu");
+        }, 1500);
+      }
+      
     } catch (error) {
-      console.log(error);
+      console.error("Error:", error.response ? error.response.data : error.message);
+      if (error.response && error.response.status === 401) {
+        localStorage.clear();
+        navigate("/login");
+      } else {
+        Swal.fire({
+          icon: "error",
+          title: "Gagal menambahkan barang",
+          text: error.response?.data?.message || "Terjadi kesalahan",
+          showConfirmButton: false,
+          timer: 1500,
+        });
+      }
     }
   };
 
   return (
-    <div style={{ padding: "50px", backgroundColor: "#f9f9f9", borderRadius: "8px", boxShadow: "0 4px 8px rgba(0, 0, 0, 0.66)", width: "50%", margin: "auto" }}>
+    <div
+      style={{
+        padding: "50px",
+        backgroundColor: "#f9f9f9",
+        borderRadius: "8px",
+        boxShadow: "0 4px 8px rgba(0, 0, 0, 0.66)",
+        width: "50%",
+        margin: "auto",
+      }}
+    >
       <h1 style={{ textAlign: "center", color: "#333" }}>Form Tambah Data</h1>
 
       <Form onSubmit={addData}>
@@ -54,7 +131,12 @@ function Add() {
             onChange={(e) => setName(e.target.value)}
             type="text"
             placeholder="Nama Produk"
-            style={{ borderRadius: "4px", padding: "6px", margin: "7px", boxShadow: "0 3px 8px rgba(0, 0, 0, 0.19)" }}
+            style={{
+              borderRadius: "4px",
+              padding: "6px",
+              margin: "7px",
+              boxShadow: "0 3px 8px rgba(0, 0, 0, 0.19)",
+            }}
           />
         </Form.Group>
 
@@ -66,7 +148,12 @@ function Add() {
             value={type}
             onChange={(e) => setType(e.target.value)}
             placeholder="Tipe Produk"
-            style={{ borderRadius: "4px", padding: "6px", margin: "7px", boxShadow: "0 3px 8px rgba(0, 0, 0, 0.19)" }}
+            style={{
+              borderRadius: "4px",
+              padding: "6px",
+              margin: "7px",
+              boxShadow: "0 3px 8px rgba(0, 0, 0, 0.19)",
+            }}
           />
         </Form.Group>
 
@@ -79,7 +166,12 @@ function Add() {
             value={price}
             onChange={(e) => setPrice(e.target.value)}
             placeholder="Harga"
-            style={{ borderRadius: "4px", padding: "6px", margin: "7px", boxShadow: "0 3px 8px rgba(0, 0, 0, 0.19)" }}
+            style={{
+              borderRadius: "4px",
+              padding: "6px",
+              margin: "7px",
+              boxShadow: "0 3px 8px rgba(0, 0, 0, 0.19)",
+            }}
           />
         </Form.Group>
 
@@ -92,20 +184,30 @@ function Add() {
             value={deskripsi}
             onChange={(e) => setDeskripsi(e.target.value)}
             placeholder="Deskripsi"
-            style={{ borderRadius: "4px", padding: "6px", margin: "7px", boxShadow: "0 3px 8px rgba(0, 0, 0, 0.19)" }}
+            style={{
+              borderRadius: "4px",
+              padding: "6px",
+              margin: "7px",
+              boxShadow: "0 3px 8px rgba(0, 0, 0, 0.19)",
+            }}
           />
         </Form.Group>
 
         <Form.Group className="mb-3">
           <Form.Label style={{ fontWeight: "bold" }}>Link Gambar</Form.Label>
           <Form.Control
-            type="text"
+            type="file"
             name="link_gambar"
             id="link_gambar"
-            value={link_gambar}
-            onChange={(e) => setGambar(e.target.value)}
+            accept="image/*"
+            onChange={(e) => setGambar(e.target.files[0])}
             placeholder="Link Gambar"
-            style={{ borderRadius: "4px", padding: "6px", margin: "7px", boxShadow: "0 3px 8px rgba(0, 0, 0, 0.19)" }}
+            style={{
+              borderRadius: "4px",
+              padding: "6px",
+              margin: "7px",
+              boxShadow: "0 3px 8px rgba(0, 0, 0, 0.19)",
+            }}
           />
         </Form.Group>
 
@@ -123,8 +225,12 @@ function Add() {
         >
           Submit
         </Button>
-        <a className="text-white ml-72 bg-blue-500 font-semibold py-3 px-12 rounded-md transition duration-200 ease-in-out transform hover:bg-blue-700 hover:scale-105 focus:outline-none focus:ring-2 focus:ring-blue-500"
-       href="/menu">Kembali</a>
+        <a
+          className="text-white ml-72 bg-blue-500 font-semibold py-3 px-12 rounded-md transition duration-200 ease-in-out transform hover:bg-blue-700 hover:scale-105 focus:outline-none focus:ring-2 focus:ring-blue-500"
+          href="/menu"
+        >
+          Kembali
+        </a>
       </Form>
     </div>
   );
